89527c75b0ee24aa789eb8f44c9d4299c08a1508
Federico <fedee.unates.2001@gmail.com>
2024-04-06 23:02:55 -0300

    clase Pasajero creada con sus getters y setters

---
+++ Pasajero/Pasajero.php
@@ -0,0 +1,57 @@
+<?php
+
+class Pasajero
+{
+  //variables
+  private $nombre;
+  private $apellido;
+  private $doc;
+  private $tel;
+
+  //getters
+  public function getNombre()
+  {
+    return $this->nombre;
+  }
+  public function getApellido()
+  {
+    return $this->apellido;
+  }
+  public function getDoc()
+  {
+    return $this->doc;
+  }
+  public function getTel()
+  {
+    return $this->tel;
+  }
+  //setters
+  public function setNombre($nuevoNombre)
+  {
+    $this->nombre = $nuevoNombre;
+  }
+  public function setApellido($nuevoApellido)
+  {
+    $this->apellido = $nuevoApellido;
+  }
+  public function setDoc($nuevoDoc)
+  {
+    $this->doc = $nuevoDoc;
+  }
+  public function setTel($nuevoTel)
+  {
+    $this->tel = $nuevoTel;
+  }
+
+  public function __toString()
+  {
+    return $this->getNombre()
+      . " "
+      . $this->getApellido()
+      . "\n"
+      . $this->getDoc()
+      . "\n"
+      . $this->getTel()
+      . "\n";
+  }
+}
